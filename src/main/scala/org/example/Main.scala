57092dba478de6ef78408574758da6bc235a9164
Anant Khaitan <anant.khaitan@avalara.com>
2019-04-10 11:55:39 +0530

    Failing with enums

--- src/main/scala/org/example/Main.scala
+++ src/main/scala/org/example/Main.scala
@@ -3,19 +3,18 @@ package org.example
 import java.io.ByteArrayOutputStream
 import java.nio.ByteBuffer
 
+import org.apache.avro.Schema
 import org.apache.avro.io.{BinaryEncoder, DecoderFactory, EncoderFactory}
 import org.apache.avro.specific.{SpecificData, SpecificDatumReader, SpecificDatumWriter, SpecificRecord}
 
-import scala.reflect.ClassTag
-import scala.util.Try
 import scala.collection.JavaConversions._
+import scala.reflect.ClassTag
 
 object Main {
   def main(args: Array[String]): Unit = {
     val version1 = new v1.DNode("p", "n")
     val version2 = new v2.DNode("p", "n", 0l)
-    val version3 =
-      new v3.DNode("p", "n", 0l, ByteBuffer.wrap(Array(2.asInstanceOf[Byte])))
+    val version3 = new v3.DNode("p", "n", 0l, ByteBuffer.wrap(Array(2.asInstanceOf[Byte])))
     compareWith[v1.DNode](version2) // case where-in we have more data than we need
     compareWith[v2.DNode](version1) // need the patch for 1 extra field(s)
     compareWith[v3.DNode](version1) // need the patch for 2 extra field(s)
@@ -27,12 +26,31 @@ object Main {
 
     compareWith[v1.SiteTags](siteTagsV2)
     compareWith[v2.SiteTags](siteTagsV1)
+
+    val urlMetasV0 = new v0.URLMetas(
+      Map(
+        "t1" -> new v0.EuclidMeta(
+          new v0.SeederState("start", "1"),
+          null,
+          null,
+          v0.PageType.LISTING_DISCOVERY,
+          null,
+          1,
+          0,
+          null,
+          "t1",
+          null,
+          null
+        )
+      )
+    )
+
+    compareWith[v1.URLMetas](urlMetasV0)
   }
 
-  def compareWith[To <: SpecificRecord: ClassTag](
-      from: SpecificRecord): Unit = {
+  def compareWith[To <: SpecificRecord : ClassTag](from: SpecificRecord): Unit = {
     val fromBytes = serialize(from)
-    val to = deserialize[To](fromBytes)
+    val to = deserialize[To](fromBytes, from.getSchema)
     println(s"from : $from --> to: $to")
   }
 
@@ -46,12 +64,11 @@ object Main {
     out.toByteArray
   }
 
-  def deserialize[T <: SpecificRecord: ClassTag](bytes: Array[Byte]): T = {
+  def deserialize[T <: SpecificRecord : ClassTag](bytes: Array[Byte], readerSchema: Schema): T = {
     val classTag = implicitly[ClassTag[T]]
-    val reader = new SpecificDatumReader[T](
-      SpecificData.get.getSchema(classTag.runtimeClass))
+    val reader = new SpecificDatumReader[T](readerSchema, SpecificData.get.getSchema(classTag.runtimeClass))
     val decoder = DecoderFactory.get().binaryDecoder(bytes, null)
     val newObj = classTag.runtimeClass.asInstanceOf[Class[T]].newInstance()
-    Try(reader.read(newObj, decoder)).getOrElse(newObj)
+    reader.read(newObj, decoder)
   }
 }
